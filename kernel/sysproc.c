9a03f46ed79c2bb9bbda397ad63f96cc4a6b72c5
chocolatedesue <91049859+chocolatedesue@users.noreply.github.com>
2022-07-25 11:04:14 +0000

    copy the systrace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,6 +7,19 @@
 #include "spinlock.h"
 #include "proc.h"
 
+
+ uint64
+ sys_trace(void)
+ {
+ int mask;
+
+ if(argint(0, &mask) < 0)
+     return -1;
+	
+ myproc()->syscall_trace = mask;
+ return 0;
+ }
+ 
 uint64
 sys_exit(void)
 {
